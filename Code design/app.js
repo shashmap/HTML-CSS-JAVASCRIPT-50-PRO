ddfd4319fa48ea3af8a9d954bb499b75d21a7b03
Shashma <shashmap18@gmail.com>
2024-11-12 19:35:59 +0530

    Read less/Read more project Added

---
+++ Code design/app.js
@@ -0,0 +1,25 @@
+const questions = [
+    {
+        question:"Which is the largest animal in the world?",
+        answers:[
+            {text:"shark",correct: false},
+            {text:"Blue whale",correct: true},
+            {text:"Elephant",correct: false},
+            {text:"Giraffe",correct: false},
+
+
+        ]
+    },
+    {
+        question:"Which is the smallest continent in the world?",
+        answers:[
+            {text:"Asia",correct: false},
+            {text:"Australia",correct: true},
+            {text:"Arctic",correct: false},
+            {text:"Africa",correct: false},
+
+
+        ]
+    }
+];
+
