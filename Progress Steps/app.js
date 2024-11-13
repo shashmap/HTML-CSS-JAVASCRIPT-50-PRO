113726ce8e1863e2e2317702e708ea14897b37bd
Shashma <shashmap18@gmail.com>
2024-11-13 21:25:14 +0530

    Progress  Section Project Added

---
+++ Progress Steps/app.js
@@ -0,0 +1,52 @@
+const progress = document.getElementById("progress");
+const prevBtn= document.getElementById("prev");
+const nextbtn = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+
+let activeIndex=1;
+
+nextbtn.addEventListener('click',()=>{
+activeIndex++;
+if(activeIndex> circles.length){
+    activeIndex = circles.length;
+}
+updateUI();
+});
+
+prevBtn.addEventListener('click', ()=>{
+activeIndex--;
+if(activeIndex< 1){
+    activeIndex = 1;
+}
+updateUI();
+});
+
+function updateUI(){
+circles.forEach((circle,index) => {
+if(index < activeIndex){
+    circle.classList.add("active");
+}
+else{
+    circle.classList.remove("active");
+  
+}
+});
+const actives =document.querySelectorAll(".active");
+progress.style.width=((actives.length -1)/ (circles.length -1))*100+'%'
+
+if(activeIndex===1){
+    prevBtn.disabled=true;
+}
+else if(activeIndex===circles.length){
+    nextbtn.disabled=true;
+
+}
+else{
+    prevBtn.disabled=false;
+    nextbtn.disabled=false;
+
+}
+}
+
+
